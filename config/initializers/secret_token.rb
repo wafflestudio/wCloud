c66f3e4d96bed6b85ab8d75090403ecd59aef4a6
Taekmin Kim <tantara.tm@gmail.com>
2013-02-09 15:32:03 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WCloud::Application.config.secret_token = 'ab5c5405db62a41b0c9118fb7313d8f0b06f87c5de68545963bbbdfb4a7b32d67e4009900a70d7071c42a768bd713f34935c4999ea747665b24824ce3a831979'
