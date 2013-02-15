77afcd652c2b01cf082480059d0eac4363b0c879
Taekmin Kim <tantara.tm@gmail.com>
2013-02-15 09:03:22 -0500

    add pjax

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,6 @@ module WCloud
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.middleware.use Rack::Pjax
   end
 end
